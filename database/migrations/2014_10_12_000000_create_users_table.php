cce2a4f7317f84c8f54481c3e036fa1cc45d3f24
zhueryan <zhuan99974@gmail.com>
2018-12-08 07:35:43 +0800

    创建user表

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+//            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->comment('图像');
+            $table->string('confirmation_token')->comment('邮箱验证字段');
+            $table->smallInteger('is_active')->comment('是否激活了邮箱');
+            $table->integer('questions_count')->default(0)->comment('问题数');
+            $table->integer('answers_count')->default(0)->comment('回答数');
+            $table->integer('comments_count')->default(0)->comment('评论数');
+            $table->integer('favorites_count')->default(0)->comment('收藏数');
+            $table->integer('likes_count')->default(0)->comment('点赞数');
+            $table->integer('followers_count')->default(0)->comment('关注数');
+            $table->integer('following_count')->default(0)->comment('被关注数');
+            $table->json('settings')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
